3d5e088e168fbc46816d5c4175bd866383d36374
brenthore <franzisbugna@yahoo.com>
2020-11-28 20:06:04 +0800

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -13,10 +13,34 @@ import { auth } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser:null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+ 
+
   render() {
     return(
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
